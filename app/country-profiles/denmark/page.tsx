a2b2b076d0630cd4641a202282a5533c6113b439
Simon Mravec <simon.mravec@gmail.com>
2025-01-07 10:01:57 +0100

    Economy country profile

--- app/country-profiles/denmark/page.tsx
+++ app/country-profiles/denmark/page.tsx
@@ -17,31 +17,61 @@ export default function Denmark(){
             <Title order={2} mb={"lg"}>
                 Economy
             </Title>
-            <Table variant="vertical" layout="fixed" withTableBorder>
+            <Table variant="vertical" layout="fixed" >
                 <Table.Tbody>
                     <Table.Tr>
                     <Table.Th >Gdp per capita</Table.Th>
-                    <Table.Td> {Math.trunc(CountryData["Denmark"]["2023"]["Gdp per capita"])}</Table.Td>
+                    <Table.Td> {Math.trunc(CountryData["Denmark"]["2023"]["Gdp per capita"])} €</Table.Td>
                     </Table.Tr>
 
                     <Table.Tr>
                     <Table.Th>Gdp per capita PPP</Table.Th>
-                    <Table.Td> {Math.trunc(CountryData["Denmark"]["2023"]["Gdp per capita PPP"])}</Table.Td>
+                    <Table.Td> {Math.trunc(CountryData["Denmark"]["2023"]["Gdp per capita PPP"])} €</Table.Td>
                     </Table.Tr>
 
                     <Table.Tr>
-                    <Table.Th>Total issues</Table.Th>
-                    <Table.Td>135</Table.Td>
+                    <Table.Th>Net average wage</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Net average wage"])} €</Table.Td>
                     </Table.Tr>
 
                     <Table.Tr>
-                    <Table.Th>Total story points</Table.Th>
-                    <Table.Td>874</Table.Td>
+                    <Table.Th>Unemployment</Table.Th>
+                    <Table.Td>{CountryData["Denmark"]["2023"]["Unemployment"]} %</Table.Td>
                     </Table.Tr>
 
                     <Table.Tr>
-                    <Table.Th>Last updated at</Table.Th>
-                    <Table.Td>September 26, 2024 17:41:26</Table.Td>
+                    <Table.Th>Home affordability</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Home affordability"])}</Table.Td>
+                    </Table.Tr>
+
+                    <Table.Tr>
+                    <Table.Th>Gdp growth past 5 years</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Gdp growth past 5 years"])} %</Table.Td>
+                    </Table.Tr>
+
+                    <Table.Tr>
+                    <Table.Th>Debt to gdp</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Government debt"])} %</Table.Td>
+                    </Table.Tr>
+
+                    <Table.Tr>
+                    <Table.Th>Corporate tax</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Corporate tax"])} %</Table.Td>
+                    </Table.Tr>
+
+                    <Table.Tr>
+                    <Table.Th>Capital gains tax</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Capital gains tax"])} %</Table.Td>
+                    </Table.Tr>
+                    
+                    <Table.Tr>
+                    <Table.Th>Value added tax</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2023"]["Value added tax"])} %</Table.Td>
+                    </Table.Tr>
+
+                    <Table.Tr>
+                    <Table.Th>R&D (Innovation) spending</Table.Th>
+                    <Table.Td>{Math.trunc(CountryData["Denmark"]["2021"]["R&D (Innovation) spending"] * 100) / 100} %</Table.Td>
                     </Table.Tr>
                 </Table.Tbody>
             </Table>
@@ -51,7 +81,7 @@ export default function Denmark(){
                 Society
             </Title>
             
-            <Table variant="vertical" layout="fixed" withTableBorder>
+            <Table variant="vertical" layout="fixed" >
                 <Table.Tbody>
                     <Table.Tr>
                     <Table.Th > Trust in other people</Table.Th>
