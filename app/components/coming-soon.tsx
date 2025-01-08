d5d33717da7bd2ff867c98dbc6005080f49e65bf
Simon Mravec <simon.mravec@gmail.com>
2025-01-09 00:31:53 +0100

    finished pages info

--- app/components/coming-soon.tsx
+++ app/components/coming-soon.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Flex, Title, Button } from "@mantine/core"
+import { Flex, Title, Button, Text } from "@mantine/core"
 import { useRouter} from "next/navigation"
 
 export default function ComingSoon(){
@@ -11,9 +11,12 @@ export default function ComingSoon(){
         align={"center"}
         justify={"center"}
         mt={"30vh"}>
-            <Title mb={"sm"}>
+            <Title>
                 Coming soon
             </Title>
+            <Text mb={"sm"}>
+                Finished pages: Country profiles
+            </Text>
 
             <Button onClick={()=>{router.push("/")}}>
                 Back to main page
