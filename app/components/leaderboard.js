fff1b1de9b1abdca639ff3577f69d3e34353579d
Simon Mravec <simon.mravec@gmail.com>
2025-01-10 23:13:32 +0100

    Fix font size

--- app/components/leaderboard.js
+++ app/components/leaderboard.js
@@ -59,7 +59,7 @@ export default function Leaderboard(props){
         <Table.Tr>
             {data.headers.map((item, index) => 
                 index === 0 ? (
-                    <Table.Th key={item} w="10%" bg="main" c="white" fz={"18px"} >{item}</Table.Th>
+                    <Table.Th key={item} w="10%" bg="main" c="white" fz={"16px"} >{item}</Table.Th>
                 ) : (
                     <Table.Th 
                         key={item} 
@@ -67,7 +67,7 @@ export default function Leaderboard(props){
                         c="white"
                         ta={index === 2 ? "right" : "left"}
                         pr={index === 2 ? "10px": "0px"}
-                        fz={"18px"}
+                        fz={"16px"}
                     >
                         {item}
                     </Table.Th>
