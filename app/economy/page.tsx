4e7118784fccf7210435853f3650a264643920ff
Simon Mravec <simon.mravec@gmail.com>
2025-01-19 17:39:58 +0100

    Methodology economy

--- app/economy/page.tsx
+++ app/economy/page.tsx
@@ -1,12 +1,65 @@
 'use client'
 
 import Link from "next/link";
-import { Title, useMantineTheme, Flex,SimpleGrid, Anchor } from "@mantine/core";
+import { Title, useMantineTheme, Flex,SimpleGrid, Anchor, Text } from "@mantine/core";
 
 import OwnScore from "../components/own-score"
 
 export default function Economy(){
     const theme = useMantineTheme()
+
+    const methodology =[
+        [
+            "Gdp per capita", 
+            "How much money is yearly in the economy per average person",
+            "imf"
+        ],
+        [
+            "Gdp per capita PPP",
+            "Gdp per capita adjusted to local prices (how much can the average person buy)",
+            "imf"
+        ],
+        [
+            "Net average monthly wage",
+            "Average monthly salary after tax",
+            "eurostat"
+        ],
+        [
+            "Unemployment",
+            "Unemployment rate (in %)",
+            "eurostat"
+        ],
+        [
+            "House price to wage",
+            "Average house price to average salary",
+            "eurostat, other ..."
+        ],
+        [
+            "Gdp growth past 5 years",
+            "How much the gdp has grown past 5 years (in %)",
+            "imf"
+        ],
+        [
+            "Debt to gdp",
+            "National debt compared to gdp (in %)",
+            "imf"
+        ],
+        [
+            "Corporate tax",
+            "Tax on corporate profits (in %)",
+            "wikipedia, other ..."
+        ],
+        [
+            "Capital gains tax",
+            "Tax on gains of stocks (in %)",
+            "wikipedia, other ..."
+        ],
+        [
+            "Value added tax",
+            "Tax on goods and services",
+            "wikipedia, other ..."
+        ]
+    ]
 
     return(
         <>
@@ -43,6 +96,40 @@ export default function Economy(){
                 <Anchor component={Link}  style={{fontSize: "20px", whiteSpace: "nowrap"}} href="/economy/rd-innovation-to-gdp">R&D (Innovation) to gdp</Anchor>
             </SimpleGrid> 
         </Flex>
+
+        <Flex 
+                bg={theme.colors.gray[2]}
+                direction={"column"}
+                align={"center"}
+                justify={"center"}
+                w={"100%"}
+                pt={"15px"}
+                pb={"20px"}
+                style={{borderRadius: "8px"}}
+                bd={"1px solid"  + theme.colors.gray[4]}
+                mt={"sm"}
+                pl={"20px"}
+                >
+                    <Title mb={"lg"}>Methodology</Title>
+        
+                    <SimpleGrid
+                    cols={{base: 1, sm:2 , md: 2, lg: 4}}
+                    verticalSpacing={"5px"}
+                    spacing={"20px"}
+                    >
+                        {
+                            methodology.map((item,index)=>(
+                                <Flex key={index} direction={"column"} maw={"200px"} mr={"10px"} mb={"8px"}>
+                                <Title order={4} ml={"0"} >{item[0]}</Title>
+                                <Text size="sm">
+                                    {item[1]}
+                                </Text>
+                                <Text  size= "sm" fs={"italic"}>Source: {item[2]}</Text>
+                            </Flex>
+                            ))
+                        }
+                    </SimpleGrid>
+            </Flex>
         </>
     )
 }
